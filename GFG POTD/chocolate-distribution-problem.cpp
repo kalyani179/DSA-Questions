655cc06a621f8c70f7209533748edcddc8cd23ea
kalyani179 <117893404+kalyani179@users.noreply.github.com>
2023-08-05 22:54:52 +0530

    potd

--- GFG POTD/chocolate-distribution-problem.cpp
+++ GFG POTD/chocolate-distribution-problem.cpp
@@ -3,11 +3,11 @@ class Solution{
     long long findMinDiff(vector<long long> a, long long n, long long m){
         
         sort(a.begin(),a.end());
-        long long d=INT_MAX;
+        long long diff=INT_MAX;
         int i=0,j=m-1;
         for(int i=0;j<n;i++,j++){
-            d = min(d,a[j]-a[i]);
+            diff = min(diff,a[j]-a[i]);
         }
-        return d;
+        return diff;
     }   
 };
